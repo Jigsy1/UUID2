40922cab5198bdd9222f3bd1ff944039eb8bbc3d
Jigsy <34282672+Jigsy1@users.noreply.github.com>
2022-03-11 04:19:17 +0000

    Add files via upload
    Third time's the charm!
    
    1. Fixed an issue with an array hence why I was using range(33) instead of range(32). (I thought that was odd...) Now back at 32.
    2. Also changed to f string at the advice of somebody.
    3. Other minor changes.

--- UUID2.py
+++ UUID2.py
@@ -1,4 +1,4 @@
-# A python 3 script to generate an alpha numeric "UUID"
+# A python 3 script to generate an alpha numeric "UUID."
 
 import random
 import string
@@ -10,24 +10,24 @@ uuid2 = Tk()
 uuid2.title("UUID2")
 uuid2.resizable(height=False, width=False)
 
-def genUUID2():
+def makeID():
   str = ""
-  stdout = ""
-  str = ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(33))
-  stdout = "{}-{}-{}-{}-{}".format(str[0:8], str[8:12], str[12:16], str[16:20], str[21:])
+  out = ""
+  str = "".join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(32))
+  out = f"{str[0:8]}-{str[8:12]}-{str[12:16]}-{str[16:20]}-{str[20:]}"
   if braces.get() == 1:
-    stdout = "{" + stdout + "}"
-  print(stdout)
+    out = "{" + out + "}"
+  print(out)
   uuid2.clipboard_clear()
-  uuid2.clipboard_append(stdout)
+  uuid2.clipboard_append(out)
   uuid2.update()
 
 braces = IntVar(value=1)
 
-braceCheck = Checkbutton(uuid2, text="{Use Braces}", variable=braces, onvalue=1, offvalue=0)
-genButton = Button(uuid2, text="Generate", command=genUUID2)
+braceBox = Checkbutton(uuid2, text="{Use Braces}", variable=braces, onvalue=1, offvalue=0)
+genButton = Button(uuid2, text="Generate", command=makeID)
 
-braceCheck.pack(side = RIGHT)
+braceBox.pack(side = RIGHT)
 genButton.pack(side = LEFT)
 
 uuid2.mainloop()
