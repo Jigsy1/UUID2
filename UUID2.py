71010cfb96cdb7f7043323172f145bdd34970d8d
Jigsy <34282672+Jigsy1@users.noreply.github.com>
2022-03-11 01:07:14 +0000

    Add files via upload

---
+++ UUID2.py
@@ -0,0 +1,38 @@
+# A python 3 script to generate an alpha numeric "UUID"
+
+import random
+import string
+import tkinter
+
+from tkinter import *
+
+uuid2 = Tk()
+uuid2.title("UUID2")
+uuid2.resizable(height=False, width=False)
+
+def genUUID2():
+  len = 0
+  str = ""
+  stdout = ""
+  while (len <= 32):
+    str = str + random.choice(string.ascii_uppercase + string.digits)
+    len += 1
+  stdout = str[0:8] + "-" + str[8:12] + "-" + str[12:16] + "-" + str[16:20] + "-" + str[21:]
+  if braces.get() == 1:
+    stdout = "{" + stdout + "}"
+  print(stdout)
+  uuid2.clipboard_clear()
+  uuid2.clipboard_append(stdout)
+  uuid2.update()
+
+braces = IntVar(value=1)
+
+braceCheck = Checkbutton(uuid2, text="{Use Braces}", variable=braces, onvalue=1, offvalue=0)
+genButton = Button(uuid2, text="Generate", command=genUUID2)
+
+braceCheck.pack(side = RIGHT)
+genButton.pack(side = LEFT)
+
+uuid2.mainloop()
+
+# EOF
